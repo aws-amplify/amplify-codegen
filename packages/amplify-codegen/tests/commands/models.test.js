11cb697a506eeb7d407ef61dfb7101cdfc8aca76
phani-srikar <phanisrikar93ume@gmail.com>
2021-03-22 19:29:34 -0700

    fix(amplify-codegen): remove commented test

--- packages/amplify-codegen/tests/commands/models.test.js
+++ packages/amplify-codegen/tests/commands/models.test.js
@@ -15,7 +15,6 @@ const MOCK_PROJECT_ROOT = 'project';
 const MOCK_PROJECT_NAME = 'myapp';
 const MOCK_BACKEND_DIRECTORY = 'backend';
 const MOCK_GENERATED_CODE = 'This code is auto-generated!';
-const MOCK_PRE_EXISTING_FILE = 'preexisting.txt';
 
 // Mock the Feature flag to use migrated moldegen
 jest.mock('amplify-cli-core', (MOCK_PROJECT_ROOT) => {
@@ -298,92 +297,6 @@ describe('command-models adds Amplify CLI version comment', () => {
 
   afterEach(mockFs.restore);
 });
-
-/*
-describe('codegen models respects cleanGeneratedModelsDirectory', () => {
-  beforeEach(() => {
-    jest.resetAllMocks();
-    addMocksToContext();
-    graphqlCodegen.codegen.mockReturnValue(MOCK_GENERATED_CODE);
-  });
- 
-  for (const frontend in OUTPUT_PATHS) {
-    it(frontend + ': Should clear the output directory before generation if cleanGeneratedModelsDirectory is true', async () => {
-      MOCK_CONTEXT.amplify.getProjectConfig.mockReturnValue({frontend: frontend});
-      require('amplify-cli-core').FeatureFlags.getBoolean.mockImplementation((name, defaultValue) => {
-          if (name === 'codegen.useappsyncmodelgenplugin' ||
-              name === 'codegen.cleanGeneratedModelsDirectory') {
-            return true;
-          }
-      });
-
-      const outputDirectory = path.join(MOCK_PROJECT_ROOT, OUTPUT_PATHS[frontend]);
-      const preExistingFilePath = path.join(outputDirectory, MOCK_PRE_EXISTING_FILE);
-      createMockFS(outputDirectory);
-
-      // assert output directory has a mock file to be cleared
-      expect(fs.readdirSync(outputDirectory).length).toEqual(1);
-      expect(fs.existsSync(preExistingFilePath));
-
-      await generateModels(MOCK_CONTEXT);
-
-      // assert model generation succeeds
-      expect(graphqlCodegen.codegen).toBeCalled();
-
-      // assert that codegen generated in correct place
-      expect(fs.readdirSync(outputDirectory).length).toBeGreaterThan(0);
-
-      // assert that the pre-existing file is deleted
-      expect(fs.existsSync(preExistingFilePath)).toBeFalsy;
-    });
-
-    it(frontend + ': Should not clear the output directory before generation if cleanGeneratedModelsDirectory is false', async () => {
-      MOCK_CONTEXT.amplify.getProjectConfig.mockReturnValue({frontend: frontend});
-      require('amplify-cli-core').FeatureFlags.getBoolean.mockImplementation((name, defaultValue) => {
-          if (name === 'codegen.useappsyncmodelgenplugin') {
-            return true;
-          }
-          if (name === 'codegen.cleanGeneratedModelsDirectory') {
-              return false;
-          }
-      });
-
-      const outputDirectory = path.join(MOCK_PROJECT_ROOT, OUTPUT_PATHS[frontend]);
-      const preExistingFilePath = path.join(outputDirectory, MOCK_PRE_EXISTING_FILE);
-      createMockFS(outputDirectory);
-
-      // assert output directory has a mock file to be cleared
-      expect(fs.readdirSync(outputDirectory).length).toEqual(1);
-      expect(fs.existsSync(preExistingFilePath));
-
-      await generateModels(MOCK_CONTEXT);
- 
-      // assert model generation succeeds
-      expect(graphqlCodegen.codegen).toBeCalled();
- 
-      // assert that codegen generated in correct place
-      expect(fs.readdirSync(outputDirectory).length).toBeGreaterThan(1);
- 
-      // assert that the pre-existing file is retained
-      expect(fs.existsSync(preExistingFilePath)).toBeTruthy;
-    });
-  }
-
-  function createMockFS(outputDirectory) {
-    // mock the input and output file structure
-    const schemaFilePath = path.join(MOCK_BACKEND_DIRECTORY, 'api', MOCK_PROJECT_NAME);
-    const mockedFiles = {};
-    mockedFiles[schemaFilePath] = {
-        'schema.graphql': ' type SimpleModel { id: ID! status: String } '
-    };
-    mockedFiles[outputDirectory] = {};
-    mockedFiles[outputDirectory][MOCK_PRE_EXISTING_FILE] = 'A pre-existing mock file';
-    mockFs(mockedFiles);
-  }
-  
-  afterEach(mockFs.restore);
-}); 
-*/
 
 // Add models generation specific mocks to Amplify Context
 function addMocksToContext(context, frontend) {
