e0cd22546eeab3bce7321b341b36f178fc3faa58
Zeyu Li <lizeyu@amazon.com>
2021-04-02 11:01:23 -0400

    fix lint error

--- packages/amplify-codegen/src/walkthrough/questions/languageTarget.js
+++ packages/amplify-codegen/src/walkthrough/questions/languageTarget.js
@@ -15,7 +15,7 @@ async function askCodeGenTargetLanguage(context, target, withoutInit = false, de
   if (!withoutInit) {
     frontend = getFrontEndHandler(context);
   }
-  
+
   //flutter only supports modelgen but the amplify graphql codegen
   if (frontend === 'flutter') {
     throw new AmplifyCodeGenNotSupportedError(constants.ERROR_FLUTTER_CODEGEN_NOT_SUPPORTED);
