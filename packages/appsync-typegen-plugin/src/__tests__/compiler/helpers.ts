41313a1759653091b1478a5a051912805f0b7802
Adireddy <sadiredd@3c22fb12605c.ant.amazon.com>
2021-04-12 16:01:09 -0700

    fix: changes to helper file

--- packages/appsync-typegen-plugin/src/__tests__/compiler/helpers.ts
+++ packages/appsync-typegen-plugin/src/__tests__/compiler/helpers.ts
@@ -1,14 +1,10 @@
-import { parse, GraphQLSchema } from 'graphql';
-import { compileToIR, CompilerOptions } from '../../compiler';
-import { loadSchema } from '../../loading';
+// import { parse, GraphQLSchema } from 'graphql';
+// import { compileToIR, CompilerOptions } from '../../compiler';
+// import { loadSchema } from '../../loading';
 
-export const starWarsSchema = loadSchema(require.resolve('../starwarsschema.json'));
+// export const starWarsSchema = loadSchema(require.resolve('../starwarsschema.json'));
 
-export function compile(source: string, schema: GraphQLSchema = starWarsSchema, options: CompilerOptions = {}) {
-  const document = parse(source);
-  return compileToIR(schema, document, options);
-}
-
-it('sample test in core plugin', async () => {
-  expect('generate').toEqual('generate');
-});
+// export function compile(source: string, schema: GraphQLSchema = starWarsSchema, options: CompilerOptions = {}) {
+//   const document = parse(source);
+//   return compileToIR(schema, document, options);
+// }
