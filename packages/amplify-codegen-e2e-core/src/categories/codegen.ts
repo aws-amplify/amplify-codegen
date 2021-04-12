ea57ee816cd7bdab3f838d684686a36b4f48e901
phani-srikar <phanisrikar93ume@gmail.com>
2021-04-12 10:03:48 -0700

    fix(e2e-testing): remove select plugin step from codegen models prompts

--- packages/amplify-codegen-e2e-core/src/categories/codegen.ts
+++ packages/amplify-codegen-e2e-core/src/categories/codegen.ts
@@ -3,8 +3,6 @@ import { getCLIPath, nspawn as spawn } from '..';
 export function generateModels(cwd: string): Promise<void> {
   return new Promise((resolve, reject) => {
     spawn(getCLIPath(), ['codegen', 'models'], { cwd, stripColors: true })
-    .wait('Select the plugin module to execute')
-    .sendCarriageReturn()
     .run((err: Error) => {
       if (!err) {
         resolve();
