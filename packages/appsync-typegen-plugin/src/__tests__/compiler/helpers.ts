701f262cd98137fcfa67c7a6f123baa3dedc148f
Adireddy <sadiredd@3c22fb12605c.ant.amazon.com>
2021-04-12 16:55:49 -0700

    fix: code coverage file changed in package.json

---
+++ packages/appsync-typegen-plugin/src/__tests__/compiler/helpers.ts
@@ -0,0 +1,10 @@
+// import { parse, GraphQLSchema } from 'graphql';
+// import { compileToIR, CompilerOptions } from '../../compiler';
+// import { loadSchema } from '../../loading';
+
+// export const starWarsSchema = loadSchema(require.resolve('../starwarsschema.json'));
+
+// export function compile(source: string, schema: GraphQLSchema = starWarsSchema, options: CompilerOptions = {}) {
+//   const document = parse(source);
+//   return compileToIR(schema, document, options);
+// }
