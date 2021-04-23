f303c291e4a6f9763fb6bf04a4bd1a70f908acab
phani-srikar <phanisrikar93ume@gmail.com>
2021-04-23 10:36:36 -0700

    ci(e2e-testing): add tests for codegen add and configure workflows

--- packages/amplify-codegen-e2e-core/src/categories/codegen.ts
+++ packages/amplify-codegen-e2e-core/src/categories/codegen.ts
@@ -27,27 +27,38 @@ export function generateStatementsAndTypes(cwd: string) : Promise<void> {
   });
 }
 
+// CLI workflow to add codegen to Amplify project
 export function addCodegen(cwd: string, settings: any = {}): Promise<void> {
   return new Promise((resolve, reject) => {
-    const run = spawn(getCLIPath(), ['codegen', 'add'], { cwd, stripColors: true });
-    if (settings.frontendType === AmplifyFrontend.javascript) {
-      run.wait('Choose the code generation language target').sendCarriageReturn();
+    const chain = spawn(getCLIPath(), ['codegen', 'add'], { cwd, stripColors: true });
+    if (settings.isAPINotAdded) {
+      chain.wait("There are no GraphQL APIs available.");
+      chain.wait("Add by running $amplify api add");
     }
-    run
-      .wait('Enter the file name pattern of graphql queries, mutations and subscriptions')
-      .sendCarriageReturn()
-      .wait('Do you want to generate/update all possible GraphQL operations')
-      .sendLine('y')
-      .wait('Enter maximum statement depth [increase from default if your schema is deeply')
-      .sendCarriageReturn();
-    if (settings.frontendType === AmplifyFrontend.ios) {
-      run
-        .wait('Enter the file name for the generated code')
+    else if (settings.isCodegenAdded) {
+      chain.wait("Codegen support only one GraphQL API per project");
+    }
+    else {
+      if (settings.frontendType === AmplifyFrontend.javascript) {
+        chain.wait('Choose the code generation language target').sendCarriageReturn();
+      }
+      chain
+        .wait('Enter the file name pattern of graphql queries, mutations and subscriptions')
         .sendCarriageReturn()
-        .wait('Do you want to generate code for your newly created GraphQL API')
+        .wait('Do you want to generate/update all possible GraphQL operations')
+        .sendLine('y')
+        .wait('Enter maximum statement depth [increase from default if your schema is deeply')
         .sendCarriageReturn();
+      if (settings.frontendType === AmplifyFrontend.ios) {
+        chain
+          .wait('Enter the file name for the generated code')
+          .sendCarriageReturn()
+          .wait('Do you want to generate code for your newly created GraphQL API')
+          .sendCarriageReturn();
+      }
     }
-    run.run((err: Error) => {
+
+    chain.run((err: Error) => {
       if (!err) {
         resolve();
       } else {
@@ -73,3 +84,52 @@ export function removeCodegen(cwd: string, isCodegenAdded: boolean = true): Prom
     });
   });
 }
+
+// CLI workflow to configure codegen
+export function configureCodegen(cwd: string, settings: any = {}): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const chain = spawn(getCLIPath(), ['codegen', 'configure'], { cwd, stripColors: true });
+    if (settings.frontendType === AmplifyFrontend.javascript) {
+      chain.wait('Choose the code generation language target').sendCarriageReturn();
+    }
+    chain
+      .wait('Enter the file name pattern of graphql queries, mutations and subscriptions')
+      .sendCarriageReturn()
+
+    if (!settings.isCodegenConfigured) {
+      chain
+        .wait('Do you want to generate/update all possible GraphQL operations')
+        .sendLine('y')
+    }
+    else if (settings.frontendType === AmplifyFrontend.ios) {
+      chain
+        .wait('Enter the file name for the generated code')
+        .sendCarriageReturn()
+    }
+
+    chain
+      .wait('Enter maximum statement depth [increase from default if your schema is deeply')
+    settings.maxStatementDepth ? chain.sendLine(settings.maxStatementDepth) : chain.sendCarriageReturn();
+    
+    if (settings.frontendType === AmplifyFrontend.ios && !settings.isCodegenConfigured) {
+      chain
+        .wait('Enter the file name for the generated code')
+        .sendCarriageReturn()
+        .wait('Do you want to generate code for your newly created GraphQL API')
+        .sendCarriageReturn();
+    }
+
+    if (settings.isCodegenConfigured) {
+      chain.wait("Codegen configured. Remember to run \"amplify codegen\" to generate your types and statements.");
+    }
+
+    chain.run((err: Error) => {
+      if (!err) {
+        resolve();
+      } else {
+        reject(err);
+      }
+    });
+  });
+}
+
