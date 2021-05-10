67d3dc44a061cc19a5f5dd99f2e23c70c7a7773c
Phani Srikar Edupuganti <55896475+phani-srikar@users.noreply.github.com>
2021-05-10 09:42:50 -0700

    Extend build and test jobs to windows and mac OS (#157)
    * ci: add windows and mac images
    
    * ci: fix windows image resource class type
    
    * ci: testing nvm install macos
    
    * fix(amplify-codegen): run unit tests from windows
    
    * ci(e2e-testing): changes to make unit tests run on windows
    
    * ci(graphql-types-generator): fix the tests on windows for typegen
    
    * ci(e2e-testing): use node 12 for macOS
    
    * ci(e2e-testing): store caches for different executors using unique keys
    
    * ci(e2e-testing): run single e2e test on all executors
    
    * ci(e2e-testing): fix permissions issue to run yarn on macOS
    
    * ci(e2e-testing): add exit 0 to terminate install cli script windows
    
    * ci(e2e-testing): remove windows e2e testing job
    
    * ci(e2e-testing): remove macos e2e job
    
    * ci(e2e-testing): remove feature branch from build-test-deploy workflow
    
    * ci(e2e-testing): remove macos specific step
    
    * ci: modify install_cli step
    
    * ci(e2e-testing): upgrade minimum node version to 12
    
    Co-authored-by: edupp <edupp@CORP.AMAZONWORKSPACES.COM>

--- packages/amplify-codegen/tests/utils/getGraphQLDocPath.test.js
+++ packages/amplify-codegen/tests/utils/getGraphQLDocPath.test.js
@@ -1,16 +1,19 @@
 const getGraphQLDocPath = require('../../src/utils/getGraphQLDocPath');
-const getIncludePatterns = require('../../src/utils/getIncludePattern')
+const getIncludePatterns = require('../../src/utils/getIncludePattern');
+const { join } = require('path');
 
 describe('getGraphQLDocPath', () => {
   it('should return com/amplify/generated/graphql when used in android project', () => {
     const SCHEMA_LOCATION = 'app/src/main/graphql/schema.json';
     const graphQLDirectory = getIncludePatterns('android', SCHEMA_LOCATION).graphQLDirectory;
-    expect(getGraphQLDocPath('android', graphQLDirectory)).toEqual('app/src/main/graphql/com/amazonaws/amplify/generated/graphql');
+    expect(getGraphQLDocPath('android', graphQLDirectory)).toEqual(
+      join('app', 'src', 'main', 'graphql', 'com', 'amazonaws', 'amplify', 'generated', 'graphql'),
+    );
   });
 
   it('should return default folder when frontend is not android and include path not defined', () => {
     const graphQLDirectory = getIncludePatterns('javascript').graphQLDirectory;
-    expect(getGraphQLDocPath('javascript', graphQLDirectory)).toEqual('src/graphql');
+    expect(getGraphQLDocPath('javascript', graphQLDirectory)).toEqual(join('src', 'graphql'));
   });
 
   it('should return parent folder for include glob path when frontend is not android and include path is defined', () => {
