7edbe97e1f8f46988e2b6d4220f13226461ecd1a
phani-srikar <phanisrikar93ume@gmail.com>
2021-05-14 16:29:42 -0700

    chore(e2e-testing): fix failing feature flags test

--- packages/amplify-codegen-e2e-tests/src/__tests__/feature-flags.test.ts
+++ packages/amplify-codegen-e2e-tests/src/__tests__/feature-flags.test.ts
@@ -3,6 +3,7 @@ import { initJSProjectWithProfile, createNewProjectDir, deleteProject, deletePro
 import { pathManager } from 'amplify-cli-core';
 
 const codegenFeatureFlags = {
+    "addtimestampfields": true,
     "useappsyncmodelgenplugin": true,
     "usedocsgeneratorplugin": true,
     "usetypesgeneratorplugin": true,
