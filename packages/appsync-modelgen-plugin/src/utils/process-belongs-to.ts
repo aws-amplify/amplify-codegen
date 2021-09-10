7f454e09488fca36d7406fbd0b234da1df2e6827
Marc VandenBerg <marc.vandenberg1@gmail.com>
2021-09-10 12:57:18 -0400

    Fix: Remove unreachable code and add some error checking belongsTo

--- packages/appsync-modelgen-plugin/src/utils/process-belongs-to.ts
+++ packages/appsync-modelgen-plugin/src/utils/process-belongs-to.ts
@@ -22,49 +22,39 @@ export function processBelongsToConnection(
     throw new Error('DataStore only support one key in field');
   }
 
-  // if a type is connected using name, then graphql-connection-transformer adds a field to
+  if (field.isList) {
+    throw new Error(
+      `A list field does not support the 'belongsTo' relation`
+    );
+  }
+  else if (field.isNullable && otherSideField.isNullable) {
+    throw new Error(
+      `DataStore does not support 1 to 1 connection with both sides of connection as optional field: ${model.name}.${field.name}`,
+    );
+  }
+
+  let validOtherSideField = false;
+  otherSideField.directives.forEach(dir => {
+    if (dir.name === 'hasOne' || dir.name === 'hasMany') {
+      validOtherSideField = true;
+    }
+  });
+
+  if (!validOtherSideField) {
+    throw new Error(
+      `A 'belongsTo' field should match to a corresponding 'hasMany' or 'hasOne' field`
+    );
+  }
+  // if a type is connected using name, then amplify-graphql-relational-transformer adds a field to
   //  track the connection and that field is not part of the selection set
   // but if the field are connected using fields argument in connection directive
   // we are reusing the field and it should be preserved in selection set
   const isConnectingFieldAutoCreated = connectionFields.length === 0;
 
-  if (!field.isList && otherSideField.isList) {
-      //  One to Many
-    return {
-      kind: CodeGenConnectionType.BELONGS_TO,
-      connectedModel: otherSide,
-      isConnectingFieldAutoCreated,
-      targetName: connectionFields[0] || makeConnectionAttributeName(model.name, field.name),
-    };
-  }
-  else if (!field.isList && !otherSideField.isList) {
-    if (!field.isNullable && otherSideField.isNullable) {
-      return {
-        kind: CodeGenConnectionType.BELONGS_TO,
-        connectedModel: otherSide,
-        isConnectingFieldAutoCreated,
-        targetName: connectionFields[0] || makeConnectionAttributeName(model.name, field.name),
-      };
-    }
-    else {
-      throw new Error(
-        `DataStore does not support 1 to 1 connection with both sides of connection as optional field: ${model.name}.${field.name}`,
-      );
-    }
-  }
-  else {
-    if (!field.isList) {
-      return {
-        kind: CodeGenConnectionType.BELONGS_TO,
-        connectedModel: otherSide,
-        isConnectingFieldAutoCreated,
-        targetName: connectionFields[0] || makeConnectionAttributeName(model.name, field.name),
-      };
-    }
-    else {
-      throw new Error(
-        `A list field does not support the 'belongsTo' relation`
-      );
-    }
-  }
+  return {
+    kind: CodeGenConnectionType.BELONGS_TO,
+    connectedModel: otherSide,
+    isConnectingFieldAutoCreated,
+    targetName: connectionFields[0] || makeConnectionAttributeName(model.name, field.name),
+  };
 }
