02aacdd4fbf5676d869ed438f04657b1e14f9edc
Jordan Nelson <nejrd@amazon.com>
2021-09-30 15:09:08 -0400

    feat: update ignored rules, and linting note

--- packages/appsync-modelgen-plugin/src/visitors/appsync-dart-visitor.ts
+++ packages/appsync-modelgen-plugin/src/visitors/appsync-dart-visitor.ts
@@ -18,6 +18,7 @@ import {
   DART_RESERVED_KEYWORDS,
   typeToEnumMap,
   IGNORE_FOR_FILE,
+  CUSTOM_LINTS_MESSAGE,
 } from '../configs/dart-config';
 import dartStyle from 'dart-style';
 import { generateLicense } from '../utils/generateLicense';
@@ -95,6 +96,8 @@ export class AppSyncModelDartVisitor<
     //License
     const license = generateLicense();
     result.push(license);
+    //Custom lints warning
+    result.push(CUSTOM_LINTS_MESSAGE);
     //Ignore for file
     result.push(IGNORE_FOR_FILE);
     //Packages for import
@@ -139,6 +142,8 @@ export class AppSyncModelDartVisitor<
     //License
     const license = generateLicense();
     result.push(license);
+    //Custom lints warning
+    result.push(CUSTOM_LINTS_MESSAGE);
     //Ignore for file
     result.push(IGNORE_FOR_FILE);
     //Enum
@@ -157,6 +162,8 @@ export class AppSyncModelDartVisitor<
     //License
     const license = generateLicense();
     result.push(license);
+    //Custom lints warning
+    result.push(CUSTOM_LINTS_MESSAGE);
     //Ignore for file
     result.push(IGNORE_FOR_FILE);
     //Imports
