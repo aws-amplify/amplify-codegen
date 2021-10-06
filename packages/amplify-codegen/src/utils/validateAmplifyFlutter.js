c98b9838d061a75ebbee81ccd002b15a3451c59e
Zeyu Li <lizeyu@amazon.com>
2021-10-06 11:43:10 -0700

    fix: add prerelease version and local path check

--- packages/amplify-codegen/src/utils/validateAmplifyFlutter.js
+++ packages/amplify-codegen/src/utils/validateAmplifyFlutter.js
@@ -8,8 +8,13 @@ const { PUBSPEC_FILE_NAME } = require('./validateDartSDK');
 function validateAmplifyFlutter(projectRoot) {
   try {
     const config = yaml.load(fs.readFileSync(path.join(projectRoot, PUBSPEC_FILE_NAME), 'utf8'));
+    //path exists for local testing
+    if (config.dependencies.amplify_flutter.path) {
+      return true;
+    }
+    //check dependency version
     const version = semver.minVersion(config.dependencies.amplify_flutter);
-    if (semver.satisfies(version, '>= 0.3.0')) {
+    if (semver.satisfies(version, '>= 0.3.0 || >= 0.3.0-rc.1')) {
       return true;
     }
     return false;
--- packages/amplify-codegen/tests/utils/validateAmplifyFlutter.test.js
+++ packages/amplify-codegen/tests/utils/validateAmplifyFlutter.test.js
@@ -40,6 +40,15 @@ describe('Validate amplify flutter version tests', () => {
       mockFs({ [MOCK_PUBSPEC_FILE_PATH]: yaml.dump(config) });
       expect(validateAmplifyFlutter(MOCK_PROJECT_ROOT)).toBe(true);
     });
+    it('with prerelease version', () => {
+      const config = {
+        dependencies: {
+          amplify_flutter: '0.3.0-rc.1',
+        },
+      };
+      mockFs({ [MOCK_PUBSPEC_FILE_PATH]: yaml.dump(config) });
+      expect(validateAmplifyFlutter(MOCK_PROJECT_ROOT)).toBe(true);
+    });
   });
 
   describe('should return false if the minimum version is less than 0.3.0', () => {
@@ -70,11 +79,32 @@ describe('Validate amplify flutter version tests', () => {
       mockFs({ [MOCK_PUBSPEC_FILE_PATH]: yaml.dump(config) });
       expect(validateAmplifyFlutter(MOCK_PROJECT_ROOT)).toBe(false);
     });
+    it('with prerelease version', () => {
+      const config = {
+        dependencies: {
+          amplify_flutter: '0.3.0-rc.0',
+        },
+      };
+      mockFs({ [MOCK_PUBSPEC_FILE_PATH]: yaml.dump(config) });
+      expect(validateAmplifyFlutter(MOCK_PROJECT_ROOT)).toBe(false);
+    });
   });
 
   it('should return false if the sdk version cannot be found', () => {
     const config = {};
     mockFs({ [MOCK_PUBSPEC_FILE_PATH]: yaml.dump(config) });
     expect(validateAmplifyFlutter(MOCK_PROJECT_ROOT)).toBe(false);
+  });
+
+  it('should return true if the path is defined in amplify_flutter', () => {
+    const config = {
+      dependencies: {
+        amplify_flutter: {
+          path: '../../amplify_flutter'
+        }
+      },
+    };
+    mockFs({ [MOCK_PUBSPEC_FILE_PATH]: yaml.dump(config) });
+    expect(validateAmplifyFlutter(MOCK_PROJECT_ROOT)).toBe(true);
   });
 });
