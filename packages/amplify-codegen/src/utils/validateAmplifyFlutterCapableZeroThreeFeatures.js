2e462235ca401d5caef3a2f0985ec8a71b1e935a
Hui Zhao <10602282+HuiSF@users.noreply.github.com>
2021-12-07 13:54:36 -0800

    feat(appsync-dart-visitor): feat: amplify-flutter preview release (#302)
    * feat(appsync-modelgen-plugin): Genearting dart class for CustomType (non-model) (#234)
    
    * feat(appsync-dart-visior): Genearting dart class for CustomType (non-model)
    
    * Fix ModelProvider switch block
    
    * Update unit test snapshot
    
    * Adding copyWith and toString method to non-model in Dart
    
    * feat(amplify-codegen): add amplify flutter library check for non model generation (#247)
    
    * feat(amplify-codegen): add amplify flutter library check for non model generation
    
    * fix: add frontend check
    
    * test: add unit tests for validate amplify flutter
    
    * fix: add prerelease version and local path check
    
    * fix: bump prerelease to rc2
    
    * fix: remove local path check
    
    * feat(appsync-modelgen-plugin): add readOnly fields in dart (#263)
    
    * feat(appsync-modelgen-plugin): add readOnly fields in dart
    
    * fix: obsolete snapshots
    
    * feat(appsync-dart-visitor): insert auth provider info (#272)
    
    * feat(appsync-dart-visitor): insert auth provider info
    
    * Remove unused var
    
    * chore(appsyc-dart-visitor): Update test util function signature
    
    * Update test snapshot files to match the latest change
    
    * Keep original behvaior when enableDartNonModelGeneration set to false
    
    * Print errors when loading yaml file fails
    
    * Improve amplify_flutter library version check
    
    - Enable auth provider for amplify-flutter > 0.3.0
    
    - Enable timestamp fields for amplify-flutter > 0.3.0
    
    * Apply latest linter notes to generate custom type classes
    
    Co-authored-by: Zeyu Li <lizeyu@amazon.com>
    Co-authored-by: Phani Srikar Edupuganti <55896475+phani-srikar@users.noreply.github.com>

---
+++ packages/amplify-codegen/src/utils/validateAmplifyFlutterCapableZeroThreeFeatures.js
@@ -0,0 +1,33 @@
+const yaml = require('js-yaml');
+const path = require('path');
+const fs = require('fs-extra');
+const semver = require('semver');
+const { printer } = require('amplify-prompts');
+
+const PUBSPEC_LOCK_FILE_NAME = 'pubspec.lock';
+const MINIMUM_VERSION_CONSTRAIN = '>= 0.3.0 || >= 0.3.0-rc.2';
+
+function validateAmplifyFlutterCapableZeroThreeFeatures(projectRoot) {
+  try {
+    const lockFile = yaml.load(fs.readFileSync(path.join(projectRoot, PUBSPEC_LOCK_FILE_NAME), 'utf8'));
+    // check resolved dependency version written pubspec.lock file
+    const { version } = lockFile.packages.amplify_flutter || {};
+    // For this util function it check only if the amplify_flutter version is great than the minimum version
+    // and it's not concerned with prerelease range, hence including prerelease to ensure
+    // 0.4.0-rc.2 > 0.3.0-rc.2 is true
+    if (semver.satisfies(version, MINIMUM_VERSION_CONSTRAIN, { includePrerelease: true })) {
+      return true;
+    }
+    return false;
+  } catch (e) {
+    if (e.stack) {
+      printer.error(e.stack);
+      printer.error(e.message);
+    }
+
+    printer.error('An error occurred while parsing ' + PUBSPEC_LOCK_FILE_NAME + '.');
+    return false;
+  }
+}
+
+module.exports = { validateAmplifyFlutterCapableZeroThreeFeatures, PUBSPEC_LOCK_FILE_NAME, MINIMUM_VERSION_CONSTRAIN };
