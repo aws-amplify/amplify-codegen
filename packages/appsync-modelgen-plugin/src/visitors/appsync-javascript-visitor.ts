fa067e09409d13848a89ab7141df2d2287d92bf2
Zeyu Li <lizeyu@amazon.com>
2022-01-11 15:47:04 -0800

    feat(modelgen): Custom primary key support (#351)
    * test: disable flow modern tests
    
    * feat(appsync-modelgen-plugin): add custom primary key support
    
    * test: add unit tests for pk info
    
    * fix: add comment
    
    * fix: use one import instead

--- packages/appsync-modelgen-plugin/src/visitors/appsync-javascript-visitor.ts
+++ packages/appsync-modelgen-plugin/src/visitors/appsync-javascript-visitor.ts
@@ -32,7 +32,9 @@ export class AppSyncModelJavascriptVisitor<
   TRawConfig extends RawAppSyncModelJavaScriptConfig = RawAppSyncModelJavaScriptConfig,
   TPluginConfig extends ParsedAppSyncModelJavaScriptConfig = ParsedAppSyncModelJavaScriptConfig
 > extends AppSyncModelTypeScriptVisitor<TRawConfig, TPluginConfig> {
-  protected IMPORT_STATEMENTS = ['import { ModelInit, MutableModel, PersistentModelConstructor } from "@aws-amplify/datastore";'];
+  protected IMPORT_STATEMENTS = [
+    'import { ModelInit, MutableModel, PersistentModelConstructor, ManagedIdentifier, OptionallyManagedIdentifier, CustomIdentifier } from "@aws-amplify/datastore";',
+  ];
 
   constructor(
     schema: GraphQLSchema,
@@ -62,15 +64,11 @@ export class AppSyncModelJavascriptVisitor<
         .map(typeObj => this.generateModelDeclaration(typeObj, true))
         .join('\n\n');
 
-      if (!this.config.isTimestampFieldsAdded) {
-        return [imports, enumDeclarations, nonModelDeclarations, modelDeclarations].join('\n\n');
-      } else {
-        const modelMetaData = Object.values(this.modelMap)
-          .map(typeObj => this.generateModelMetaData(typeObj))
-          .join('\n\n');
+      const modelMetaData = Object.values(this.modelMap)
+        .map(typeObj => this.generateModelMetaData(typeObj))
+        .join('\n\n');
 
-        return [imports, enumDeclarations, nonModelDeclarations, modelMetaData, modelDeclarations].join('\n\n');
-      }
+      return [imports, enumDeclarations, nonModelDeclarations, modelMetaData, modelDeclarations].join('\n\n');
     } else {
       const imports = this.generateImportsJavaScriptImplementation();
       const enumDeclarations = Object.values(this.enumMap)
