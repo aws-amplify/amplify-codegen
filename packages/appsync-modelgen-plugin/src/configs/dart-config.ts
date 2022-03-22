b23186364a35132c2745135da2c460f179132c10
Hui Zhao <zhohz@amazon.com>
2022-03-22 15:30:57 -0700

    fix(appsync-dart-visitor): update ignored linter rules

--- packages/appsync-modelgen-plugin/src/configs/dart-config.ts
+++ packages/appsync-modelgen-plugin/src/configs/dart-config.ts
@@ -9,7 +9,7 @@ export const CUSTOM_LINTS_MESSAGE = `// NOTE: This file is generated and may not
 // For more info, see: https://dart.dev/guides/language/analysis-options#excluding-code-from-analysis`;
 
 export const IGNORE_FOR_FILE =
-  '// ignore_for_file: public_member_api_docs, file_names, unnecessary_new, prefer_if_null_operators, prefer_const_constructors, slash_for_doc_comments, annotate_overrides, non_constant_identifier_names, unnecessary_string_interpolations, prefer_adjacent_string_concatenation, unnecessary_const, dead_code';
+         '// ignore_for_file: public_member_api_docs, file_names, unnecessary_new, prefer_if_null_operators, prefer_const_constructors, slash_for_doc_comments, annotate_overrides, non_constant_identifier_names, unnecessary_string_interpolations, prefer_adjacent_string_concatenation, unnecessary_const, dead_codepublic_member_api_docs, file_names, unnecessary_new, prefer_if_null_operators, prefer_const_constructors, slash_for_doc_comments, annotate_overrides, non_constant_identifier_names, unnecessary_string_interpolations, prefer_adjacent_string_concatenation, unnecessary_const, dead_code, sort_child_properties_last, use_build_context_synchronously, depend_on_referenced_packages, library_private_types_in_public_api, no_leading_underscores_for_library_prefixes, no_leading_underscores_for_local_identifiers, null_check_on_nullable_type_parameter, prefer_interpolation_to_compose_strings, unnecessary_constructor_name, unnecessary_late, unnecessary_null_aware_assignments, unnecessary_nullable_for_final_variable_declarations';
 
 export const typeToEnumMap: Record<string, string> = {
   String: '.string',
