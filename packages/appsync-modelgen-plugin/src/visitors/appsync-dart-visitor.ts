5c3827f3b2e462b7ae000f64f10f8747d0cdb586
Hui Zhao <10602282+HuiSF@users.noreply.github.com>
2022-06-16 13:09:18 -0700

    feat(appsync-dart-visitor): capability of creating query predicate wi… (#441)
    * feat(appsync-dart-visitor): capability of creating query predicate with custom PK
    
    * wrap the code generation with the feature flag

--- packages/appsync-modelgen-plugin/src/visitors/appsync-dart-visitor.ts
+++ packages/appsync-modelgen-plugin/src/visitors/appsync-dart-visitor.ts
@@ -874,7 +874,19 @@ export class AppSyncModelDartVisitor<
   }
 
   protected generateModelSchema(model: CodeGenModel, classDeclarationBlock: DartDeclarationBlock): void {
+    const modelName = model.name;
     const schemaDeclarationBlock = new DartDeclarationBlock();
+
+    if (this.isCustomPKEnabled()) {
+      // QueryField that allows creating query predicate with custom PK
+      schemaDeclarationBlock.addClassMember(
+        'MODEL_IDENTIFIER',
+        `QueryModelIdentifier<${modelName}ModelIdentifier>`,
+        `QueryModelIdentifier<${modelName}ModelIdentifier>()`,
+        { static: true, final: true },
+      );
+    }
+
     //QueryField
     this.getWritableFields(model).forEach(field => {
       this.generateQueryField(model, field, schemaDeclarationBlock);
