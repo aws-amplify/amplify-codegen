983866bc546c512c6473ae00c07cda741d037a71
Zeyu Li <lizeyu@amazon.com>
2022-08-25 16:30:35 -0700

    add command unit test

--- packages/amplify-codegen/commands/codegen/model-introspection.js
+++ packages/amplify-codegen/commands/codegen/model-introspection.js
@@ -1,19 +1,12 @@
 const codeGen = require('../../src');
 const { exitOnNextTick } = require('amplify-cli-core');
 const featureName = 'model-introspection';
-const path = require('path');
 
 module.exports = {
   name: featureName,
   run: async context => {
     try {
-      // Verify override path flag is provided
-      const outputDirParam = context.parameters.options ? context.parameters.options['output-dir'] : null;
-      if ( !outputDirParam ) {
-        throw new Error('Expected --output-dir flag to be set for model introspection command.');
-      }
-      const outputDirPath = path.isAbsolute(outputDirParam) ? outputDirParam : path.join(context.amplify.getEnvInfo().projectPath, outputDirParam);
-      await codeGen.generateModels(context, outputDirPath, true);
+      await codeGen.generateModelIntrospection(context);
     } catch (ex) {
       context.print.info(ex.message);
       console.log(ex.stack);
