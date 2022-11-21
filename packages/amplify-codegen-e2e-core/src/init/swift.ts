0a94f87378ae1bc7a31ec1580bd82fd91097be5e
Dane Pilcher <dppilche@amazon.com>
2022-11-21 16:01:39 -0700

    test: add swift build commands

---
+++ packages/amplify-codegen-e2e-core/src/init/swift.ts
@@ -0,0 +1,29 @@
+/* commands for an XCode Swift App */
+import { nspawn as spawn } from '..';
+
+const defaultSettings = {
+  scheme: undefined,
+  target: undefined,
+  project: undefined,
+  disableCIDetection: false,
+};
+
+export function swiftBuild(cwd: string, settings: Object = {}): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const s = { ...defaultSettings, ...settings };
+
+    const args = [];
+    if (s.scheme) args.push('-scheme', s.scheme);
+    if (s.target) args.push('-target', s.target);
+    if (s.project) args.push('-project', s.project);
+    const chain = spawn('xcodebuild', args, { cwd, stripColors: true, disableCIDetection: s.disableCIDetection, noOutputTimeout: 1000 });
+
+    chain.run((err: Error) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
