f30d16ccded4bb2d551bcdce2ba4b981e920b901
Dane Pilcher <dppilche@amazon.com>
2022-11-21 15:45:45 -0700

    ci: init swift app

---
+++ packages/amplify-codegen-e2e-tests/test-apps/swift/swift/swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftApp.swift
+//  swift
+//
+//  Created by Pilcher, Dane on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
