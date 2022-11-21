2d86e8dc688ef9a628815b03ce716daa6dfea31a
Dane Pilcher <dppilche@amazon.com>
2022-11-21 16:00:50 -0700

    ci: add amplify to swift app

--- packages/amplify-codegen-e2e-tests/test-apps/swift/swift/swiftApp.swift
+++ packages/amplify-codegen-e2e-tests/test-apps/swift/swift/swiftApp.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import Amplify
+import AWSDataStorePlugin
 
 @main
 struct swiftApp: App {
+    init() {
+        configureAmplify()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+func configureAmplify() {
+    let dataStorePlugin = AWSDataStorePlugin(modelRegistration: AmplifyModels())
+    do {
+        try Amplify.add(plugin: dataStorePlugin)
+        try Amplify.configure()
+        print("Initialized Amplify");
+    } catch {
+        // simplified error handling for the tutorial
+        print("Could not initialize Amplify: \(error)")
     }
 }
