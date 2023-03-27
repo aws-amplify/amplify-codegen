5c9df04032bbe009ede0705dc5790c269e5d6a5f
phani-srikar <phanisrikar93ume@gmail.com>
2023-03-27 14:07:07 -0700

    feat(graphql-docs-generator): remove dependencies on fs and path

--- packages/graphql-docs-generator/src/generator/index.ts
+++ packages/graphql-docs-generator/src/generator/index.ts
@@ -2,4 +2,5 @@ export * from './types';
 import generate from './generate';
 export { generateMutations, generateSubscriptions, generateQueries, lowerCaseFirstLetter } from './generateAllOperations';
 export { loadSchema } from './utils/loading';
+export { getLanguageTemplate, getTemplatePartials } from './utils/templates';
 export default generate;
