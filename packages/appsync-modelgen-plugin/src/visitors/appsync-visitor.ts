63183c56d0f46c90f6a9712b66dab07e98b9ba3c
Zeyu Li <lizeyu@amazon.com>
2023-04-12 10:04:09 -0700

    fix(introspection): primary key info when pk is belongsTo field (#570)
    * fix(introspection): primary key info when pk is belongsTo field
    
    * rename statement
    
    * update snapshot
    
    * fix lint error

--- packages/appsync-modelgen-plugin/src/visitors/appsync-visitor.ts
+++ packages/appsync-modelgen-plugin/src/visitors/appsync-visitor.ts
@@ -1028,7 +1028,9 @@ export class AppSyncModelVisitor<
             connectionInfo &&
             connectionInfo.kind !== CodeGenConnectionType.HAS_MANY &&
             connectionInfo.kind !== CodeGenConnectionType.HAS_ONE &&
-            connectionInfo.targetName !== 'id'
+            connectionInfo.targetName !== 'id' &&
+            !(this.config.target === 'introspection' &&
+              this.getFieldName(getModelPrimaryKeyComponentFields(model)[0]) === connectionInfo.targetName)
           ) {
             // Need to remove the field that is targetName
             removeFieldFromModel(model, connectionInfo.targetName);
