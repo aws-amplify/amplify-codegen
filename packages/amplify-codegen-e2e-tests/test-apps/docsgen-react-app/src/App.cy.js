ad0a0e9afe8afa760b5a7a99f1a433b6b77de340
phani-srikar <phanisrikar93ume@gmail.com>
2023-04-17 12:26:47 -0700

    chore: add cypress e2e tests in a react app

---
+++ packages/amplify-codegen-e2e-tests/test-apps/docsgen-react-app/src/App.cy.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import App from './App';
+
+describe('Testing browser compatibility of Documents Generation', () => {
+  it('generates operations for valid SDL schema and default maxDepth', async () => {
+    await testGraphQLDocumentsGeneration('sdl-schema.graphql', 2);
+  });
+
+  it('generates operations for valid Introspection schema and default maxDepth', async () => {
+    await testGraphQLDocumentsGeneration('introspection-schema.json', 2);
+  });
+});
+
+const testGraphQLDocumentsGeneration = async (schemaFileName, maxDepth) => {
+  cy.mount(<App />);
+
+  // clear all the inputs and outputs
+  cy.get('input[name="inputSchema"]').clear();
+  cy.get('input[name="maxDepth"]').clear();
+  const operations = ['queries', 'mutations', 'subscriptions'];
+  operations.map((operation) => {
+    cy.get(`textarea[name="${operation}"]`).clear();
+  });
+  const operationPrefixes = [
+    'create', 
+    'update', 
+    'delete',
+    'get',
+    'list'
+  ];
+  operationPrefixes.map((opPrefix) => {
+    cy.get(`textarea[name="${opPrefix}TodoResult"]`).clear();
+  });
+
+  // generate GraphQL documents for the compiled schema
+  const schemaString = await fetch(`/${schemaFileName}`);
+  cy.get('input[name="inputSchema"]').type(await schemaString.text());
+  cy.get('input[name="maxDepth"]').type(maxDepth);
+  cy.get('button[name="generateDocuments"]').click();
+
+  // verify that GraphQL documents are generated
+  operations.map((operation) => {
+    const outputField = cy.get(`textarea[name="${operation}"]`);
+    outputField.invoke('val').then((outputValue) => {
+      expect(outputValue).to.exist;
+      expect(outputValue).to.have.length.greaterThan(0);
+    });
+  });
+
+  // trigger GraphQL operations using generated Documents
+  cy.get('button[name="testGraphQLOperations"]').click();
+
+  // verify that GraphQL operations successfully return results
+  operationPrefixes.map((opPrefix) => {
+    const outputField = cy.get(`textarea[name="${opPrefix}TodoResult"]`);
+    outputField.invoke('val').then((outputValue) => {
+      expect(outputValue).to.exist;
+      expect(outputValue).to.have.length.greaterThan(0);
+    });
+  });
+};
