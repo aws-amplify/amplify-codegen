a81e3f688388cb136f2e569843a6895642904f6e
Zeyu Li <lizeyu@amazon.com>
2023-07-12 11:02:15 -0700

    chore: add cleanup resources script for codebuild

--- packages/amplify-codegen-e2e-core/src/utils/add-ci-tags.ts
+++ packages/amplify-codegen-e2e-core/src/utils/add-ci-tags.ts
@@ -9,6 +9,49 @@ declare global {
     }
   }
 }
+export const addCITags = (projectPath: string): void => {
+  if (process.env && process.env['CODEBUILD']) {
+    addCodeBuildCITags(projectPath);
+  }
+  else if(process.env && process.env['CIRCLECI']) {
+    addCircleCITags(projectPath);
+  }
+}
+
+/**
+ * Add CI tags for code build
+ * Refer https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-env-vars.html
+ * @param projectPath 
+ */
+export const addCodeBuildCITags = (projectPath: string): void => {
+  const tags = stateManager.getProjectTags(projectPath);
+
+  const addTagIfNotExist = (key: string, value: string): void => {
+    if (!tags.find(t => t.Key === key)) {
+      tags.push({
+        Key: key,
+        Value: value,
+      });
+    }
+  };
+  addTagIfNotExist('codebuild', sanitizeTagValue(process.env['CODEBUILD'] || 'N/A'));
+  addTagIfNotExist('codebuild:batch_build_identifier', sanitizeTagValue(process.env['CODEBUILD_BATCH_BUILD_IDENTIFIER'] || 'N/A'));
+  addTagIfNotExist('codebuild:build_id', sanitizeTagValue(process.env['CODEBUILD_BUILD_ID'] || 'N/A'));
+  // exposed by custom CLI test environment
+  if (global.getTestName) {
+    addTagIfNotExist('jest:test_name', sanitizeTagValue(global.getTestName().substr(0, 255) || 'N/A'));
+  }
+  if (global.getHookName) {
+    addTagIfNotExist('jest:hook_name', sanitizeTagValue(global.getHookName().substr(0, 255) || 'N/A'));
+  }
+  if (global.getDescibeBlocks) {
+    global.getDescibeBlocks().forEach((blockName, i) => {
+      addTagIfNotExist(`jest:describe_${i + 1}`, sanitizeTagValue(blockName.substr(0, 255) || 'N/A'));
+    });
+  }
+
+  stateManager.setProjectFileTags(projectPath, tags);
+};
 
 export const addCircleCITags = (projectPath: string): void => {
   if (process.env && process.env['CIRCLECI']) {
