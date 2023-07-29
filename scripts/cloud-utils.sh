f4d18421f30b59387334ce6ea857b977a2cfe891
phani-srikar <phanisrikar93ume@gmail.com>
2023-07-28 19:12:38 -0700

    feat: some dx improvements

--- scripts/cloud-utils.sh
+++ scripts/cloud-utils.sh
@@ -85,3 +85,30 @@ function cleanupStaleResources {
   TARGET_BRANCH=$CURR_BRANCH
   triggerProject $E2E_ACCOUNT_PROD $CLEANUP_ROLE_NAME $CLEANUP_PROFILE_NAME $CLEANUP_PROJECT_NAME $TARGET_BRANCH
 }
+
+function triggerProjectBatchWithDebugSession {
+  account_number=$1
+  role_name=$2
+  profile_name=$3
+  project_name=$4
+  target_branch=$5
+  authenticate $account_number $role_name $profile_name
+  echo AWS Account: $account_number
+  echo Project: $project_name 
+  echo Target Branch: $target_branch
+  RESULT=$(aws codebuild start-build-batch --region=$REGION --profile="${profile_name}" --project-name $project_name --source-version=$target_branch \
+    --debug-session-enabled \
+    --buildspec-override "codebuild_specs/debug_workflow.yml" \
+    --environment-variables-override name=BRANCH_NAME,value=$target_branch,type=PLAINTEXT \
+    --query 'buildBatch.id' --output text)
+  echo "https://$REGION.console.aws.amazon.com/codesuite/codebuild/$account_number/projects/$project_name/batch/$RESULT?region=$REGION"
+}
+
+function cloudE2EDebug {
+  echo Running Prod E2E Test Suite
+  E2E_ROLE_NAME=CodebuildDeveloper
+  E2E_PROFILE_NAME=AmplifyAPIE2EProd
+  E2E_PROJECT_NAME=amplify-category-api-e2e-workflow
+  TARGET_BRANCH=$CURR_BRANCH
+  triggerProjectBatchWithDebugSession $E2E_ACCOUNT_PROD $E2E_ROLE_NAME $E2E_PROFILE_NAME $E2E_PROJECT_NAME $TARGET_BRANCH
+}
--- scripts/split-e2e-tests.ts
+++ scripts/split-e2e-tests.ts
@@ -21,18 +21,20 @@ const REPO_ROOT = join(__dirname, '..');
 const TEST_TIMINGS_PATH = join(REPO_ROOT, 'scripts', 'test-timings.data.json');
 const CODEBUILD_CONFIG_BASE_PATH = join(REPO_ROOT, '.codebuild', 'e2e_workflow_base.yml');
 const CODEBUILD_GENERATE_CONFIG_PATH = join(REPO_ROOT, '.codebuild', 'e2e_workflow.yml');
+const CODEBUILD_DEBUG_CONFIG_PATH = join (REPO_ROOT, 'codebuild_specs', 'debug_workflow.yml');
 const RUN_SOLO = [];
 const EXCLUDE_TESTS = [
   'src/__tests__/build-app-swift.test.ts',
   'src/__tests__/build-app-android.test.ts',
 ];
+const DEBUG_FLAG = '--debug';
 
 export function loadConfigBase() {
   return yaml.load(fs.readFileSync(CODEBUILD_CONFIG_BASE_PATH, 'utf8'));
 }
-export function saveConfig(config: any): void {
+export function saveConfig(config: any, outputPath: string): void {
   const output = ['# auto generated file. DO NOT EDIT manually', yaml.dump(config, { noRefs: true })];
-  fs.writeFileSync(CODEBUILD_GENERATE_CONFIG_PATH, output.join('\n'));
+  fs.writeFileSync(outputPath, output.join('\n'));
 }
 export function loadTestTimings(): { timingData: { test: string; medianRuntime: number }[] } {
   return JSON.parse(fs.readFileSync(TEST_TIMINGS_PATH, 'utf-8'));
@@ -174,6 +176,7 @@ const splitTests = (
 };
 
 function main(): void {
+  const filteredTests = process.argv.slice(2);
   const configBase: any = loadConfigBase();
   const baseBuildGraph = configBase.batch['build-graph'];
   const splitE2ETests = splitTests(
@@ -189,19 +192,30 @@ function main(): void {
     (testSuites) => testSuites.filter((ts) => !EXCLUDE_TESTS.includes(ts)),
   );
 
+  let outputPath = CODEBUILD_GENERATE_CONFIG_PATH;
   let allBuilds = [...splitE2ETests];
+  if (filteredTests.length > 0) {
+    allBuilds = allBuilds.filter(build => filteredTests.includes(build.identifier));
+    if (filteredTests.includes(DEBUG_FLAG)) {
+      allBuilds = allBuilds.map(build => {
+        return { ...build, 'debug-session': true}
+      });
+      outputPath = CODEBUILD_DEBUG_CONFIG_PATH;
+    }
+  }
   const cleanupResources = {
     identifier: 'cleanup_e2e_resources',
     buildspec: '.codebuild/cleanup_e2e_resources.yml',
     env: {
       'compute-type': 'BUILD_GENERAL1_MEDIUM'
     },
-    'depend-on': [allBuilds[0].identifier]
+    'depend-on': allBuilds.length > 0 ? [allBuilds[0].identifier] : 'publish_to_local_registry',
   }
   console.log(`Total number of splitted jobs: ${allBuilds.length}`)
   let currentBatch = [...baseBuildGraph, ...allBuilds, cleanupResources];
   configBase.batch['build-graph'] = currentBatch;
-  saveConfig(configBase);
+  saveConfig(configBase, outputPath);
+  console.log(`Successfully generated the buildspec at ${outputPath}`);
 }
 
 main();
