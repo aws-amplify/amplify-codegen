4a3375a3612d9dcaddaf16eeeb9c7c91d2c06a64
Dane Pilcher <dppilche@amazon.com>
2023-08-23 13:26:03 -0600

    test: remove unnecessary mock

--- packages/amplify-codegen/tests/commands/models.test.js
+++ packages/amplify-codegen/tests/commands/models.test.js
@@ -148,38 +148,6 @@ function addMocksToContext() {
       },
     ],
   });
-  const directiveDefinition = /* GraphQl */ `
-  directive @model(
-    queries: ModelQueryMap
-    mutations: ModelMutationMap
-    subscriptions: ModelSubscriptionMap
-    timestamps: TimestampConfiguration
-  ) on OBJECT
-  input ModelMutationMap {
-    create: String
-    update: String
-    delete: String
-  }
-  input ModelQueryMap {
-    get: String
-    list: String
-  }
-  input ModelSubscriptionMap {
-    onCreate: [String]
-    onUpdate: [String]
-    onDelete: [String]
-    level: ModelSubscriptionLevel
-  }
-  enum ModelSubscriptionLevel {
-    off
-    public
-    on
-  }
-  input TimestampConfiguration {
-    createdAt: String
-    updatedAt: String
-  }
-`;
-  MOCK_CONTEXT.amplify.executeProviderUtils.mockReturnValue(directiveDefinition);
+  MOCK_CONTEXT.amplify.executeProviderUtils.mockReturnValue([]);
   MOCK_CONTEXT.amplify.pathManager.getBackendDirPath.mockReturnValue(MOCK_BACKEND_DIRECTORY);
 }
