90e519bc51133c2801aa0e4844ea0f8333e31da1
Dane Pilcher <dppilche@amazon.com>
2023-09-05 09:26:01 -0600

    test: codegen add cli

--- packages/amplify-codegen/commands/codegen/add.js
+++ packages/amplify-codegen/commands/codegen/add.js
@@ -10,11 +10,10 @@ module.exports = {
       const { options = {} } = context.parameters;
       const keys = Object.keys(options);
       // frontend and framework are undocumented, but are read when apiId is also supplied
-      const { apiId = null, region, yes, frontend, framework, ...rest } = context.parameters.options;
+      const { apiId = null, region, yes, frontend, framework, ...rest } = options;
       const extraOptions = Object.keys(rest);
-      console.log(extraOptions);
       if (extraOptions.length) {
-        const paramMsg = extraOptions.length > 1 ? 'Invalid parameters ' : 'Invalid parameter ';
+        const paramMsg = extraOptions.length > 1 ? 'Invalid parameters' : 'Invalid parameter';
         context.print.info(`${paramMsg} ${keys.join(', ')}`);
         context.print.info(constants.INFO_MESSAGE_ADD_ERROR);
         return;
