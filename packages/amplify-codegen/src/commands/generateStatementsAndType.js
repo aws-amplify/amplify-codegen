7f35f3e37bb703bc76af4acd8604d7ba933fb46f
Dane Pilcher <dppilche@amazon.com>
2023-09-13 15:35:53 -0600

    fix: codegen --apiId broken state (#689)

--- packages/amplify-codegen/src/commands/generateStatementsAndType.js
+++ packages/amplify-codegen/src/commands/generateStatementsAndType.js
@@ -4,7 +4,7 @@ const generateTypes = require('./types');
 const generateStatements = require('./statements');
 const { loadConfig } = require('../codegen-config');
 const constants = require('../constants');
-const { ensureIntrospectionSchema, getAppSyncAPIDetails } = require('../utils');
+const { ensureIntrospectionSchema, getAppSyncAPIDetails, getAppSyncAPIInfoFromProject } = require('../utils');
 const path = require('path');
 const fs = require('fs-extra');
 
@@ -17,30 +17,31 @@ async function generateStatementsAndTypes(context, forceDownloadSchema, maxDepth
     withoutInit = true;
   }
 
-  // Check if introspection schema exists
-  const schemaPath = ['schema.graphql', 'schema.json'].map(p => path.join(process.cwd(), p)).find(p => fs.existsSync(p));
-  if (withoutInit && !schemaPath) {
-    throw Error(
-      `Please download the schema.graphql or schema.json and place in ${process.cwd()} before adding codegen when not in an amplify project`
-    );
-  }
-
-  if (withoutInit) {
-    forceDownloadSchema = false;
-  }
   const config = loadConfig(context, withoutInit);
   const projects = config.getProjects();
   if (!projects.length) {
     throw new NoAppSyncAPIAvailableError(constants.ERROR_CODEGEN_NO_API_CONFIGURED);
   }
+  const project = projects[0];
+  const schemaPath = ['schema.graphql', 'schema.json'].map(p => path.join(process.cwd(), p)).find(p => fs.existsSync(p));
+  if (withoutInit && !project && !schemaPath) {
+    throw Error(
+      `Please download the schema.graphql or schema.json and place in ${process.cwd()} before adding codegen when not in an amplify project`,
+    );
+  }
+
   let apis = [];
   if (!withoutInit) {
     apis = getAppSyncAPIDetails(context);
+  } else {
+    const api = getAppSyncAPIInfoFromProject(context, project);
+    if (api) {
+      apis = [api];
+    }
   }
   if (!apis.length && !withoutInit) {
     throw new NoAppSyncAPIAvailableError(constants.ERROR_CODEGEN_NO_API_META);
   }
-  const project = projects[0];
   const { frontend } = project.amplifyExtension;
   let projectPath = process.cwd();
   if (!withoutInit) {
@@ -48,10 +49,10 @@ async function generateStatementsAndTypes(context, forceDownloadSchema, maxDepth
   }
 
   let downloadPromises;
-  if (!withoutInit) {
+  if (apis.length) {
     downloadPromises = projects.map(
       async cfg =>
-        await ensureIntrospectionSchema(context, join(projectPath, cfg.schema), apis[0], cfg.amplifyExtension.region, forceDownloadSchema)
+        await ensureIntrospectionSchema(context, join(projectPath, cfg.schema), apis[0], cfg.amplifyExtension.region, forceDownloadSchema),
     );
     await Promise.all(downloadPromises);
   }
