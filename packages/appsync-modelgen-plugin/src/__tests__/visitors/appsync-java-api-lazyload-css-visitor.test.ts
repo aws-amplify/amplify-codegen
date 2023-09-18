2ddc1a8d925ec5a495c0d4913ff245c9175b33ac
Michael Law <1365977+lawmicha@users.noreply.github.com>
2023-09-18 15:06:09 -0400

    add isDataStoreEnabled true tests

--- packages/appsync-modelgen-plugin/src/__tests__/visitors/appsync-java-api-lazyload-css-visitor.test.ts
+++ packages/appsync-modelgen-plugin/src/__tests__/visitors/appsync-java-api-lazyload-css-visitor.test.ts
@@ -17,7 +17,6 @@ const buildSchemaWithDirectives = (schema: String): GraphQLSchema => {
   return buildSchema([schema, directives, scalars].join('\n'));
 };
 
-
 const getVisitor = (
     schema: string,
     selectedType?: string,
@@ -40,101 +39,117 @@ const getVisitor = (
   return visitor;
 };
 
-const getVisitorPipelinedTransformer = (
-    schema: string,
-    selectedType?: string,
-    settings: any = {}
-) => {
-  return getVisitor(schema, selectedType, { ...settings, transformerVersion: 2 });
-};
-
 describe('AppSyncModelVisitor', () => {
 
-  it('Should generate for HasOneParent HasOneChild models', () => {
-    const schema = /* GraphQL */ `
-      type HasOneParent @model {
-        id: ID! @primaryKey
-        child: HasOneChild @hasOne
-      }
-      
-      type HasOneChild @model {
-        id: ID! @primaryKey
-        content: String
-      }
-    `;
+  const schemaHasOneParentChild = /* GraphQL */ `
+    type HasOneParent @model {
+      id: ID! @primaryKey
+      child: HasOneChild @hasOne
+    }
+    
+    type HasOneChild @model {
+      id: ID! @primaryKey
+      content: String
+    }
+  `;
 
-    const visitor = getVisitor(schema);
-    const generatedCode = visitor.generate();
-    expect(() => validateJava(generatedCode)).not.toThrow();
-    expect(generatedCode).toMatchSnapshot();
+  const schemaDefaultPKParentChild = /* GraphQL */ `
+    type DefaultPKParent @model {
+      id: ID! @primaryKey
+      content: String
+      children: [DefaultPKChild] @hasMany
+    }
+    
+    type DefaultPKChild @model {
+      id: ID! @primaryKey
+      content: String
+      parent: DefaultPKParent @belongsTo
+    }
+  `;
+
+  const schemaCompositePK = /* GraphQL */ `
+    type CompositePKParent @model {
+      customId: ID! @primaryKey(sortKeyFields:["content"])
+      content: String!
+      children: [CompositePKChild] @hasMany(indexName:"byParent", fields:["customId", "content"])
+      implicitChildren: [ImplicitChild] @hasMany
+      strangeChildren: [StrangeExplicitChild] @hasMany(indexName: "byCompositePKParentX", fields: ["customId", "content"])
+      childrenSansBelongsTo: [ChildSansBelongsTo] @hasMany
+    }
+    
+    type CompositePKChild @model {
+      childId: ID! @primaryKey(sortKeyFields:["content"])
+      content: String!
+      parent: CompositePKParent @belongsTo(fields:["parentId", "parentTitle"])
+      parentId: ID @index(name: "byParent", sortKeyFields:["parentTitle"])
+      parentTitle: String
+    }
+    
+    type ImplicitChild @model {
+      childId: ID! @primaryKey(sortKeyFields:["content"])
+      content: String!
+      parent: CompositePKParent! @belongsTo
+    }
+    
+    type StrangeExplicitChild @model {
+      strangeId: ID! @primaryKey(sortKeyFields:["content"])
+      content: String!
+      parent: CompositePKParent! @belongsTo(fields:["strangeParentId", "strangeParentTitle"])
+      strangeParentId: ID @index(name: "byCompositePKParentX", sortKeyFields:["strangeParentTitle"])
+      strangeParentTitle: String # customized foreign key for parent sort key
+    }
+    
+    type ChildSansBelongsTo @model {
+      childId: ID! @primaryKey(sortKeyFields:["content"])
+      content: String!
+      compositePKParentChildrenSansBelongsToCustomId: ID! @index(name: "byParent", sortKeyFields: ["compositePKParentChildrenSansBelongsToContent"])
+      compositePKParentChildrenSansBelongsToContent: String
+    }
+  `;
+  describe('DataStore Enabled', () => {
+    it('Should generate for HasOneParent HasOneChild models', () => {
+      const visitor = getVisitor(schemaHasOneParentChild, undefined, { isDataStoreEnabled: true });
+      const generatedCode = visitor.generate();
+      expect(() => validateJava(generatedCode)).not.toThrow();
+      expect(generatedCode).toMatchSnapshot();
+    });
+
+    it('Should generate for DefaultPKParent DefaultPKChild models', () => {
+      const visitor = getVisitor(schemaDefaultPKParentChild, undefined, { isDataStoreEnabled: true });
+      const generatedCode = visitor.generate();
+      expect(() => validateJava(generatedCode)).not.toThrow();
+      expect(generatedCode).toMatchSnapshot();
+    });
+
+    it('should generate for CompositePKParent and (CompositePK, Implicit, StrangeExplicit, ChildSansBelongsTo) Child models', () => {
+      const visitor = getVisitor(schemaCompositePK, undefined, { isDataStoreEnabled: true });
+      const generatedCode = visitor.generate();
+      expect(() => validateJava(generatedCode)).not.toThrow();
+      expect(generatedCode).toMatchSnapshot();
+    });
   });
 
-  it('Should generate for DefaultPKParent DefaultPKChild models', () => {
-    const schema = /* GraphQL */ `
-      type DefaultPKParent @model {
-        id: ID! @primaryKey
-        content: String
-        children: [DefaultPKChild] @hasMany
-      }
-      
-      type DefaultPKChild @model {
-        id: ID! @primaryKey
-        content: String
-        parent: DefaultPKParent @belongsTo
-      }
-    `;
+  describe('API only', () => {
+    it('Should generate for HasOneParent HasOneChild models', () => {
+      const visitor = getVisitor(schemaHasOneParentChild);
+      const generatedCode = visitor.generate();
+      expect(() => validateJava(generatedCode)).not.toThrow();
+      expect(generatedCode).toMatchSnapshot();
+    });
 
-    const visitor = getVisitor(schema);
-    const generatedCode = visitor.generate();
-    expect(() => validateJava(generatedCode)).not.toThrow();
-    expect(generatedCode).toMatchSnapshot();
-  });
+    it('Should generate for DefaultPKParent DefaultPKChild models', () => {
+      const visitor = getVisitor(schemaDefaultPKParentChild);
+      const generatedCode = visitor.generate();
+      expect(() => validateJava(generatedCode)).not.toThrow();
+      expect(generatedCode).toMatchSnapshot();
+    });
 
-  it('should generate for CompositePKParent and (CompositePK, Implicit, StrangeExplicit, ChildSansBelongsTo) Child models', () => {
-    const schema = /* GraphQL */ `
-      type CompositePKParent @model {
-        customId: ID! @primaryKey(sortKeyFields:["content"])
-        content: String!
-        children: [CompositePKChild] @hasMany(indexName:"byParent", fields:["customId", "content"])
-        implicitChildren: [ImplicitChild] @hasMany
-        strangeChildren: [StrangeExplicitChild] @hasMany(indexName: "byCompositePKParentX", fields: ["customId", "content"])
-        childrenSansBelongsTo: [ChildSansBelongsTo] @hasMany
-      }
-      
-      type CompositePKChild @model {
-        childId: ID! @primaryKey(sortKeyFields:["content"])
-        content: String!
-        parent: CompositePKParent @belongsTo(fields:["parentId", "parentTitle"])
-        parentId: ID @index(name: "byParent", sortKeyFields:["parentTitle"])
-        parentTitle: String
-      }
-      
-      type ImplicitChild @model {
-        childId: ID! @primaryKey(sortKeyFields:["content"])
-        content: String!
-        parent: CompositePKParent! @belongsTo
-      }
-      
-      type StrangeExplicitChild @model {
-        strangeId: ID! @primaryKey(sortKeyFields:["content"])
-        content: String!
-        parent: CompositePKParent! @belongsTo(fields:["strangeParentId", "strangeParentTitle"])
-        strangeParentId: ID @index(name: "byCompositePKParentX", sortKeyFields:["strangeParentTitle"])
-        strangeParentTitle: String # customized foreign key for parent sort key
-      }
-      
-      type ChildSansBelongsTo @model {
-        childId: ID! @primaryKey(sortKeyFields:["content"])
-        content: String!
-        compositePKParentChildrenSansBelongsToCustomId: ID! @index(name: "byParent", sortKeyFields: ["compositePKParentChildrenSansBelongsToContent"])
-        compositePKParentChildrenSansBelongsToContent: String
-      }
-    `;
-
-    const visitor = getVisitor(schema);
-    const generatedCode = visitor.generate();
-    expect(() => validateJava(generatedCode)).not.toThrow();
-    expect(generatedCode).toMatchSnapshot();
+    it('should generate for CompositePKParent and (CompositePK, Implicit, StrangeExplicit, ChildSansBelongsTo) Child models', () => {
+      const visitor = getVisitor(schemaCompositePK);
+      const generatedCode = visitor.generate();
+      expect(() => validateJava(generatedCode)).not.toThrow();
+      expect(generatedCode).toMatchSnapshot();
+    });
   });
 });
 
