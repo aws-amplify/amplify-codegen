1e484afe39a76ac633208698e3f780214819e44e
Dane Pilcher <dppilche@amazon.com>
2023-09-26 14:02:28 -0600

    fix: generate multiple swift files in graphql-generator (#718)

--- packages/graphql-generator/src/types.ts
+++ packages/graphql-generator/src/types.ts
@@ -3,7 +3,7 @@ import { generate, generateFromString } from '@aws-amplify/graphql-types-generat
 import { GenerateTypesOptions, GeneratedOutput } from './typescript';
 
 export async function generateTypes(options: GenerateTypesOptions): Promise<GeneratedOutput> {
-  const { schema, target, queries, multipleSwiftFiles = true, introspection = false } = options;
+  const { schema, target, queries, multipleSwiftFiles = false, introspection = false } = options;
 
   const generatedOutput = await generateFromString(schema, introspection, queries, target, multipleSwiftFiles, {
     addTypename: true,
