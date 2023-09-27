cdca3eb7fa8b1de395201699357105d707c7b7f3
Dane Pilcher <dppilche@amazon.com>
2023-09-27 11:52:43 -0600

    fix: set correct type for array of enum (#703)

---
+++ packages/appsync-modelgen-plugin/src/__tests__/visitors/appsync-typescript-visitor.test.ts
@@ -0,0 +1,57 @@
+import { buildSchema, GraphQLSchema, parse, visit } from 'graphql';
+import { validateTs } from '@graphql-codegen/testing';
+import { TYPESCRIPT_SCALAR_MAP } from '../../scalars';
+import { directives, scalars } from '../../scalars/supported-directives';
+import { AppSyncModelTypeScriptVisitor } from '../../visitors/appsync-typescript-visitor';
+
+const buildSchemaWithDirectives = (schema: String): GraphQLSchema => {
+  return buildSchema([schema, directives, scalars].join('\n'));
+};
+const getVisitor = (schema: string): AppSyncModelTypeScriptVisitor => {
+  const ast = parse(schema);
+  const builtSchema = buildSchemaWithDirectives(schema);
+  const visitor = new AppSyncModelTypeScriptVisitor(
+    builtSchema,
+    { directives, target: 'typescript', scalars: TYPESCRIPT_SCALAR_MAP, codegenVersion: '3.3.4' },
+    {},
+  );
+  visit(ast, { leave: visitor });
+  return visitor;
+};
+
+describe('TypeScript visitor', () => {
+  test('singular enum', () => {
+    const schema = /* GraphQL */ `
+      enum Frequency {
+        YEARLY
+        WEEKLY
+      }
+
+      type Recurrence {
+        frequency: Frequency!
+      }
+    `;
+    const visitor = getVisitor(schema);
+    expect(visitor.generate()).toMatchSnapshot();
+  });
+
+  test('list enum', () => {
+    const schema = /* GraphQL */ `
+      enum DayOfWeek {
+        MONDAY
+        TUESDAY
+        WEDNESDAY
+        THURSDAY
+        FRIDAY
+        SATURDAY
+        SUNDAY
+      }
+
+      type Recurrence {
+        daysOfWeek: [DayOfWeek!]!
+      }
+    `;
+    const visitor = getVisitor(schema);
+    expect(visitor.generate()).toMatchSnapshot();
+  });
+});
