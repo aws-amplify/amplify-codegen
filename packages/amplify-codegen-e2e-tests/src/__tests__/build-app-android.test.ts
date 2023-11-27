3c363c6e91c6dad5ff08e08284eaecce0e8d3ade
Dane Pilcher <dppilche@amazon.com>
2023-11-27 10:16:54 -0700

    test: build type and graphql statement generation (#757)

--- packages/amplify-codegen-e2e-tests/src/__tests__/build-app-android.test.ts
+++ packages/amplify-codegen-e2e-tests/src/__tests__/build-app-android.test.ts
@@ -1,35 +1,54 @@
 import {
-  initProjectWithQuickstart,
+  initProjectWithProfile,
   DEFAULT_ANDROID_CONFIG,
   updateApiSchemaWithText,
+  addApiWithDefaultSchemaAndConflictDetection,
   generateModels,
+  generateStatementsAndTypes,
   androidBuild,
-  acceptLicenses
+  acceptLicenses,
+  addCodegen,
+  AmplifyFrontend,
+  apiGqlCompile,
 } from '@aws-amplify/amplify-codegen-e2e-core';
 const { schemas } = require('@aws-amplify/graphql-schema-test-library');
-import { existsSync, writeFileSync, readdirSync, rmSync } from 'fs';
+import { existsSync, writeFileSync, readdirSync, rmSync, readFileSync } from 'fs';
 import path from 'path';
+import { parse } from 'graphql';
 
-const skip = new Set(['v2-primary-key-with-composite-sort-key', 'custom-@primaryKey-with-sort-fields']);
+const skip = new Set([
+  'v2-primary-key-with-composite-sort-key',
+  'custom-@primaryKey-with-sort-fields',
+  'v2-cyclic-has-one-dependency',
+  'v2-cyclic-has-many-dependency',
+]);
 
 describe('build app - Android', () => {
   let apiName: string;
   const projectRoot = path.resolve('test-apps/android');
   const config = DEFAULT_ANDROID_CONFIG;
   const modelDir = 'app/src/main/java/com/amplifyframework/datastore/generated/model';
+  const statementsDir = 'app/src/main/graphql/com/amazonaws/amplify/generated/graphql';
 
   beforeAll(async () => {
-    await initProjectWithQuickstart(projectRoot, { ...config });
+    await initProjectWithProfile(projectRoot, { ...config });
+    await addApiWithDefaultSchemaAndConflictDetection(projectRoot);
     apiName = readdirSync(path.join(projectRoot, 'amplify', 'backend', 'api'))[0];
+    await apiGqlCompile(projectRoot);
+    await addCodegen(projectRoot, {
+      frontendType: AmplifyFrontend.android,
+    });
     await acceptLicenses(projectRoot);
   });
 
   afterAll(async () => {
     rmSync(path.join(projectRoot, 'amplify'), { recursive: true, force: true });
+    rmSync(path.join(projectRoot, '.graphqlconfig.yml'), { recursive: true, force: true });
   });
 
   afterEach(() => {
     rmSync(path.join(projectRoot, modelDir), { recursive: true, force: true });
+    rmSync(path.join(projectRoot, statementsDir), { recursive: true, force: true });
   });
 
   Object.entries(schemas).forEach(([schemaName, schema]) => {
@@ -39,8 +58,14 @@ describe('build app - Android', () => {
       // @ts-ignore
       const schemaText = `input AMPLIFY { globalAuthRule: AuthRule = { allow: public } }\n${schema.sdl}`;
       updateApiSchemaWithText(projectRoot, apiName, schemaText);
+      await apiGqlCompile(projectRoot);
       await generateModels(projectRoot);
+      await generateStatementsAndTypes(projectRoot);
       await androidBuild(projectRoot, { ...config });
+      // android uses raw graphql syntax
+      parse(readFileSync(path.join(projectRoot, path.join(statementsDir, 'queries.graphql')), 'utf8'));
+      parse(readFileSync(path.join(projectRoot, path.join(statementsDir, 'subscriptions.graphql')), 'utf8'));
+      parse(readFileSync(path.join(projectRoot, path.join(statementsDir, 'mutations.graphql')), 'utf8'));
     };
     if (skip.has(schemaName)) {
       it.skip(testName, testFunction);
@@ -49,11 +74,38 @@ describe('build app - Android', () => {
     }
   });
 
-  it('fails build with syntax error', async () => {
+  // not supported with conflict resolution enabled
+  [
+    ['v2-cyclic-has-one-dependency', schemas['v2-cyclic-has-one-dependency']],
+    ['v2-cyclic-has-many-dependency', schemas['v2-cyclic-has-many-dependency']],
+  ].forEach(([schemaName, schema]) => {
+    // @ts-ignore
+    it(`builds models with ${schemaName}: ${schema.description}`, async () => {
+      // @ts-ignore
+      const schemaText = `input AMPLIFY { globalAuthRule: AuthRule = { allow: public } }\n${schema.sdl}`;
+      updateApiSchemaWithText(projectRoot, apiName, schemaText);
+      await generateModels(projectRoot);
+      await androidBuild(projectRoot, { ...config });
+    });
+  });
+
+  it('fails build with syntax error in models', async () => {
     // @ts-ignore
     updateApiSchemaWithText(projectRoot, apiName, Object.values(schemas)[0].sdl);
+    await apiGqlCompile(projectRoot);
     await generateModels(projectRoot);
     await writeFileSync(path.join(projectRoot, modelDir, 'AmplifyModelProvider.java'), 'foo\nbar');
     await expect(androidBuild(projectRoot, { ...config })).rejects.toThrowError();
+  });
+
+  it('fails build with syntax error in statements', async () => {
+    // @ts-ignore
+    updateApiSchemaWithText(projectRoot, apiName, Object.values(schemas)[0].sdl);
+    await apiGqlCompile(projectRoot);
+    await generateModels(projectRoot);
+    await generateStatementsAndTypes(projectRoot);
+    writeFileSync(path.join(projectRoot, statementsDir, 'mutations.graphql'), 'foo\nbar');
+    expect(() => parse(readFileSync(path.join(projectRoot, statementsDir, 'mutations.graphql'), 'utf8'))).toThrowError();
+    await androidBuild(projectRoot, { ...config });
   });
 });
--- packages/amplify-codegen-e2e-tests/src/__tests__/build-app-ts.test.ts
+++ packages/amplify-codegen-e2e-tests/src/__tests__/build-app-ts.test.ts
@@ -1,17 +1,24 @@
 import {
   initProjectWithProfile,
   DEFAULT_JS_CONFIG,
-  addApiWithBlankSchemaAndConflictDetection,
+  addApiWithDefaultSchemaAndConflictDetection,
   updateApiSchemaWithText,
   generateModels,
+  generateStatementsAndTypes,
   craInstall,
   craBuild,
+  addCodegen,
+  AmplifyFrontend,
+  apiGqlCompile,
 } from '@aws-amplify/amplify-codegen-e2e-core';
 const { schemas } = require('@aws-amplify/graphql-schema-test-library');
 import { existsSync, writeFileSync, readdirSync, rmSync } from 'fs';
 import path from 'path';
 
 const schema = 'simple_model.graphql';
+
+// not supported with conflict resolution enabled
+const skip = new Set(['v2-cyclic-has-one-dependency', 'v2-cyclic-has-many-dependency']);
 
 describe('build app - JS', () => {
   let apiName: string;
@@ -20,28 +27,63 @@ describe('build app - JS', () => {
 
   beforeAll(async () => {
     await initProjectWithProfile(projectRoot, { ...config });
-    await addApiWithBlankSchemaAndConflictDetection(projectRoot);
-    await craInstall(projectRoot, { ...config });
+    await addApiWithDefaultSchemaAndConflictDetection(projectRoot);
     apiName = readdirSync(path.join(projectRoot, 'amplify', 'backend', 'api'))[0];
+    await apiGqlCompile(projectRoot);
+    await addCodegen(projectRoot, {
+      frontendType: AmplifyFrontend.javascript,
+    });
+    await craInstall(projectRoot, { ...config });
   });
 
   afterAll(async () => {
     await rmSync(path.join(projectRoot, 'amplify'), { recursive: true, force: true });
+    rmSync(path.join(projectRoot, '.graphqlconfig.yml'), { recursive: true, force: true });
   });
 
   Object.entries(schemas).forEach(([schemaName, schema]) => {
-    // @ts-ignore
-    it(`builds with ${schemaName}: ${schema.description}`, async () => {
-      // @ts-ignore
-      const schemaText = `input AMPLIFY { globalAuthRule: AuthRule = { allow: public } }\n${schema.sdl}`;
+    const testName = `builds with ${schemaName}: ${(schema as any).description}`;
+    const testFunction = async () => {
+      const schemaText = `input AMPLIFY { globalAuthRule: AuthRule = { allow: public } }\n${(schema as any).sdl}`;
       updateApiSchemaWithText(projectRoot, apiName, schemaText);
+      await apiGqlCompile(projectRoot);
       await generateModels(projectRoot);
+      await generateStatementsAndTypes(projectRoot);
       await craBuild(projectRoot, { ...config });
-    });
+    };
+    if (skip.has(schemaName)) {
+      it.skip(testName, testFunction);
+    } else {
+      it(testName, testFunction);
+    }
   });
 
-  it('fails build with syntax error', async () => {
+  it('fails build with syntax error in models', async () => {
+    const schemaText = `input AMPLIFY { globalAuthRule: AuthRule = { allow: public } }\n${(Object.values(schemas)[0] as any).sdl}`;
+    updateApiSchemaWithText(projectRoot, apiName, schemaText);
+    await apiGqlCompile(projectRoot);
+    await generateStatementsAndTypes(projectRoot);
     await writeFileSync(path.join(projectRoot, 'src', 'models', 'index.d.ts'), 'foo\nbar');
+    await expect(craBuild(projectRoot, { ...config })).rejects.toThrowError();
+  });
+
+  it('fails build with syntax error in statements', async () => {
+    const schemaText = `input AMPLIFY { globalAuthRule: AuthRule = { allow: public } }\n${(Object.values(schemas)[0] as any).sdl}`;
+    updateApiSchemaWithText(projectRoot, apiName, schemaText);
+    await apiGqlCompile(projectRoot);
+    await generateModels(projectRoot);
+    await generateStatementsAndTypes(projectRoot);
+    await writeFileSync(path.join(projectRoot, 'src', 'graphql', 'queries.ts'), 'foo\nbar');
+    await expect(craBuild(projectRoot, { ...config })).rejects.toThrowError();
+  });
+
+  it('fails build with syntax error in types', async () => {
+    const schemaText = `input AMPLIFY { globalAuthRule: AuthRule = { allow: public } }\n${(Object.values(schemas)[0] as any).sdl}`;
+    updateApiSchemaWithText(projectRoot, apiName, schemaText);
+    await apiGqlCompile(projectRoot);
+    await generateModels(projectRoot);
+    await generateStatementsAndTypes(projectRoot);
+    await writeFileSync(path.join(projectRoot, 'src', 'API.ts'), 'foo\nbar');
     await expect(craBuild(projectRoot, { ...config })).rejects.toThrowError();
   });
 });
