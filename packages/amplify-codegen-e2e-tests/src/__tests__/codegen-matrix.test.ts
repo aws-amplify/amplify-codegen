d97aa38badc7cb4be14e7a0270ea8387656461c0
Zeyu Li <lizeyu@amazon.com>
2023-11-30 09:34:25 -0800

    add matrix test in spec

--- packages/amplify-codegen-e2e-tests/src/__tests__/codegen-matrix.test.ts
+++ packages/amplify-codegen-e2e-tests/src/__tests__/codegen-matrix.test.ts
@@ -26,7 +26,8 @@ describe('JS codegen matrix test', () => {
   afterAll(async () => {
       await deleteAmplifyProject(projectRoot);
   });
-  it('Adding codegen works as expected', async () => {
+
+  it('Flutter - no statements or types', async () => {
     config = {
       ...DEFAULT_FLUTTER_CONFIG,
       isStatementGenerated: false,
@@ -34,66 +35,86 @@ describe('JS codegen matrix test', () => {
     }
     await testAddCodegenMatrix(config, projectRoot);
   });
-  it('Adding codegen works as expected', async () => {
+
+  it('Android - no types', async () => {
     config = {
       ...DEFAULT_ANDROID_CONFIG,
       isTypeGenerated: false,
     }
     await testAddCodegenMatrix(config, projectRoot);
   });
-  it('Adding codegen works as expected', async () => {
+
+  it('iOS - single file path as type name', async () => {
     config = {
       ...DEFAULT_IOS_CONFIG,
     }
     await testAddCodegenMatrix(config, projectRoot);
   });
-  it('Adding codegen works as expected', async () => {
+  it('iOS - folder as type name', async () => {
     config = {
       ...DEFAULT_IOS_CONFIG,
       typeFileName: 'apiType', // directory name
     }
     await testAddCodegenMatrix(config, projectRoot);
   });
-  it('Adding codegen works as expected', async () => {
+
+  it('JavaScript - angular - angular', async () => {
+    config = {
+      ...DEFAULT_JS_CONFIG,
+      framework: 'angular',
+      codegenTarget: 'angular',
+    }
+    await testAddCodegenMatrix(config, projectRoot);
+  });
+  it('JavaScript - angular - typescript', async () => {
+    config = {
+      ...DEFAULT_JS_CONFIG,
+      framework: 'angular',
+      codegenTarget: 'typescript',
+    }
+    await testAddCodegenMatrix(config, projectRoot);
+  });
+
+  it('JavaScript - ionic - angular', async () => {
+    config = {
+      ...DEFAULT_JS_CONFIG,
+      framework: 'ionic',
+      codegenTarget: 'angular',
+    }
+    await testAddCodegenMatrix(config, projectRoot);
+  });
+  it('JavaScript - ionic - typescript', async () => {
+    config = {
+      ...DEFAULT_JS_CONFIG,
+      framework: 'ionic',
+      codegenTarget: 'typescript',
+    }
+    await testAddCodegenMatrix(config, projectRoot);
+  });
+
+  it('JavaScript - none - javascript', async () => {
     config = {
       ...DEFAULT_JS_CONFIG,
       isTypeGenerated: false,
     }
     await testAddCodegenMatrix(config, projectRoot);
   });
-  it('Adding codegen works as expected', async () => {
+  it('JavaScript - none - typescript', async () => {
     config = {
       ...DEFAULT_JS_CONFIG,
-      framework: 'angular',
-      codegenTarget: 'angular',
-    }
-    await testAddCodegenMatrix(config, projectRoot);
-  });
-  it('Adding codegen works as expected', async () => {
-    config = {
-      ...DEFAULT_JS_CONFIG,
-      framework: 'angular',
       codegenTarget: 'typescript',
     }
     await testAddCodegenMatrix(config, projectRoot);
   });
-  it('Adding codegen works as expected', async () => {
+  it('JavaScript - none - flow', async () => {
     config = {
       ...DEFAULT_JS_CONFIG,
-      framework: 'ionic',
-      codegenTarget: 'angular',
+      codegenTarget: 'flow',
     }
     await testAddCodegenMatrix(config, projectRoot);
   });
-  it('Adding codegen works as expected', async () => {
-    config = {
-      ...DEFAULT_JS_CONFIG,
-      framework: 'ionic',
-      codegenTarget: 'typescript',
-    }
-    await testAddCodegenMatrix(config, projectRoot);
-  });
-  it('Adding codegen works as expected', async () => {
+
+  it('JavaScript - react - javascript', async () => {
     config = {
       ...DEFAULT_JS_CONFIG,
       framework: 'react',
@@ -102,7 +123,7 @@ describe('JS codegen matrix test', () => {
     }
     await testAddCodegenMatrix(config, projectRoot);
   });
-  it('Adding codegen works as expected', async () => {
+  it('JavaScript - react - typescript', async () => {
     config = {
       ...DEFAULT_JS_CONFIG,
       framework: 'react',
@@ -110,10 +131,88 @@ describe('JS codegen matrix test', () => {
     }
     await testAddCodegenMatrix(config, projectRoot);
   });
-  it('Adding codegen works as expected', async () => {
+  it('JavaScript - react - flow', async () => {
     config = {
       ...DEFAULT_JS_CONFIG,
       framework: 'react',
+      codegenTarget: 'flow',
+    }
+    await testAddCodegenMatrix(config, projectRoot);
+  });
+
+  it('JavaScript - react native - javascript', async () => {
+    config = {
+      ...DEFAULT_JS_CONFIG,
+      framework: 'react-native',
+      codegenTarget: 'javascript',
+      isTypeGenerated: false,
+    }
+    await testAddCodegenMatrix(config, projectRoot);
+  });
+  it('JavaScript - react native - typescript', async () => {
+    config = {
+      ...DEFAULT_JS_CONFIG,
+      framework: 'react-native',
+      codegenTarget: 'typescript',
+    }
+    await testAddCodegenMatrix(config, projectRoot);
+  });
+  it('JavaScript - react native - flow', async () => {
+    config = {
+      ...DEFAULT_JS_CONFIG,
+      framework: 'react-native',
+      codegenTarget: 'flow',
+    }
+    await testAddCodegenMatrix(config, projectRoot);
+  });
+
+  it('JavaScript - ember - javascript', async () => {
+    config = {
+      ...DEFAULT_JS_CONFIG,
+      framework: 'ember',
+      codegenTarget: 'javascript',
+      isTypeGenerated: false,
+    }
+    await testAddCodegenMatrix(config, projectRoot);
+  });
+  it('JavaScript - ember - typescript', async () => {
+    config = {
+      ...DEFAULT_JS_CONFIG,
+      framework: 'ember',
+      codegenTarget: 'typescript',
+    }
+    await testAddCodegenMatrix(config, projectRoot);
+  });
+  it('JavaScript - ember - flow', async () => {
+    config = {
+      ...DEFAULT_JS_CONFIG,
+      framework: 'ember',
+      codegenTarget: 'flow',
+    }
+    await testAddCodegenMatrix(config, projectRoot);
+  });
+
+  it('JavaScript - vue - javascript', async () => {
+    config = {
+      ...DEFAULT_JS_CONFIG,
+      framework: 'vue',
+      codegenTarget: 'javascript',
+      isTypeGenerated: false,
+    }
+    await testAddCodegenMatrix(config, projectRoot);
+  });
+  it('JavaScript - vue - typescript', async () => {
+    config = {
+      ...DEFAULT_JS_CONFIG,
+      framework: 'vue',
+      codegenTarget: 'typescript',
+    }
+    await testAddCodegenMatrix(config, projectRoot);
+  });
+  it('JavaScript - vue - flow', async () => {
+    config = {
+      ...DEFAULT_JS_CONFIG,
+      framework: 'vue',
       codegenTarget: 'flow',
     }
     await testAddCodegenMatrix(config, projectRoot);
