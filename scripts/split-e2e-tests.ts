544d9195007d4e26949d10b0779bad7e41021eb8
phani-srikar <phanisrikar93ume@gmail.com>
2023-12-06 15:41:01 -0800

    chore: remove glob from utility script

--- scripts/split-e2e-tests.ts
+++ scripts/split-e2e-tests.ts
@@ -1,4 +1,4 @@
-import * as glob from 'glob';
+import { sync } from 'globby';
 import * as fs from 'fs-extra';
 import { join } from 'path';
 import * as yaml from 'js-yaml';
@@ -41,7 +41,7 @@ export function loadTestTimings(): { timingData: { test: string; medianRuntime: 
   return JSON.parse(fs.readFileSync(TEST_TIMINGS_PATH, 'utf-8'));
 }
 function getTestFiles(dir: string, pattern = 'src/**/*.test.ts'): string[] {
-  return glob.globSync(pattern, { cwd: dir });
+  return sync(pattern, { cwd: dir });
 }
 type COMPUTE_TYPE = 'BUILD_GENERAL1_MEDIUM' | 'BUILD_GENERAL1_LARGE';
 type BatchBuildJob = {
