f3f88fe0f3477caa49425bab656d9bd6fbf9767c
Dane Pilcher <dppilche@amazon.com>
2024-03-22 13:50:44 -0600

    test: don't build before test

--- packages/amplify-codegen-e2e-tests/src/__tests__/graphql-generator-app.test.ts
+++ packages/amplify-codegen-e2e-tests/src/__tests__/graphql-generator-app.test.ts
@@ -12,8 +12,6 @@ describe('GraphQL documents generator e2e tests', () => {
 
   // skip cypress test on windows
   (isWindows() ? it.skip : it)('graphql generator does not crash in browser', async () => {
-    // Build and run the cypress e2e tests
-    await craBuild(projectRoot, { ...config });
     await cypressRun(projectRoot, { componentsTesting: true });
   });
 });
