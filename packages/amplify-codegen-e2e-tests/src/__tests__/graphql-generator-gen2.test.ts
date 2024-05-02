6d319c7acb454d3dc074eef09c523b7369c5b497
Zeyu Li <lizeyu@amazon.com>
2024-05-01 18:19:03 -0700

    test: add gen2 e2e tests

---
+++ packages/amplify-codegen-e2e-tests/src/__tests__/graphql-generator-gen2.test.ts
@@ -0,0 +1,23 @@
+import { createNewProjectDir, deleteProjectDir } from "@aws-amplify/amplify-codegen-e2e-core";
+import { deleteSandbox, initGen2Project, sandboxDeploy } from "../commands";
+
+describe('GraphQL generator for Gen2 e2e tests', () => {
+  let projRoot: string;
+  let projFolderName: string;
+
+  beforeEach(async () => {
+    projFolderName = 'graphqlGeneratorGen2';
+    projRoot = await createNewProjectDir(projFolderName);
+  });
+
+  afterEach(async () => {
+    await deleteSandbox(projRoot);
+    deleteProjectDir(projRoot);
+  });
+
+  it('demo', async () => {
+    const name = await initGen2Project(projRoot);
+    console.log(name)
+    await sandboxDeploy(projRoot);
+  })
+});
---
+++ packages/amplify-codegen-e2e-tests/src/commands.ts
@@ -0,0 +1,105 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import { copySync, moveSync, readFileSync, writeFileSync } from 'fs-extra';
+import { getScriptRunnerPath, nspawn as spawn, npmInstall } from '@aws-amplify/amplify-codegen-e2e-core';
+
+/**
+ * Retrieve the path to the `npx` executable for interacting with the amplify gen2 cli.
+ * @returns the local `npx` executable path.
+ */
+const getNpxPath = (): string => (process.platform === 'win32' ? getScriptRunnerPath().replace('node.exe', 'npx.cmd') : 'npx');
+
+const codegenPackagesInGen2 = [
+  '@aws-amplify/graphql-generator',
+  '@aws-amplify/graphql-types-generator'
+];
+const apiPackagesInGen2 = [
+  '@aws-amplify/data-construct'
+];
+
+type CodegenPackage = 'GraphqlGenerator' | 'TypeGen';
+
+const codegenPackageDirectoryMap: Record<CodegenPackage, string> = {
+  GraphqlGenerator: path.join(__dirname, '..', '..', 'graphql-generator'),
+  TypeGen: path.join(__dirname, '..', '..', 'graphql-types-generator'),
+}
+
+/**
+ * Copy the backend snapshot into the generated app location.
+ */
+const copyTemplateDirectory = (projectPath: string, templatePath: string): void => {
+  const binDir = path.join(projectPath, 'bin');
+  copySync(templatePath, binDir, { overwrite: true });
+  moveSync(path.join(binDir, 'app.ts'), path.join(binDir, `${path.basename(projectPath)}.ts`), { overwrite: true });
+};
+
+/**
+ * Initialize a Amplify Gen2 project in the cwd using a reference backend `app.ts` file, and optional cdkVersion specified.
+ * @param cwd the directory to initialize the CDK project in
+ * @param templatePath path to the project to overwrite the cdk sample code with
+ * @param props additional properties to configure the test app setup.
+ * @returns a promise which resolves to the stack name
+ */
+export const initGen2Project = async (cwd: string, props: Gen2DeployProps = {}): Promise<string> => {
+  const commandOptions = {
+    cwd,
+    stripColors: true,
+  };
+
+  await spawn('npm', ['create', 'amplify@latest', '-y'], commandOptions).runAsync();
+
+  overrideWithLocalCodegenPackages(cwd);
+
+  npmInstall(cwd)
+
+  // Get root level packages info
+  await spawn('npm', ['list'], commandOptions).runAsync();
+  // Get codegen packages info
+  await spawn('npm', ['list', ...codegenPackagesInGen2], commandOptions).runAsync();
+  // Get API packages info
+  await spawn('npm', ['list', ...apiPackagesInGen2], commandOptions).runAsync();
+
+  return JSON.parse(readFileSync(path.join(cwd, 'package.json'), 'utf8')).name.replace(/_/g, '-');
+};
+
+export type Gen2DeployProps = {
+  timeoutMs?: number;
+};
+
+const overrideWithLocalCodegenPackages = (cwd: string): void => {
+  const packageJsonObj = JSON.parse(readFileSync(path.join(cwd, 'package.json'), 'utf8'));
+  packageJsonObj.overrides = {
+    '@aws-amplify/graphql-generator': codegenPackageDirectoryMap['GraphqlGenerator'],
+    '@aws-amplify/graphql-types-generator': codegenPackageDirectoryMap['TypeGen'],
+  };
+  writeFileSync(path.join(cwd, 'package.json'), JSON.stringify(packageJsonObj));
+}
+
+export const sandboxDeploy = async (cwd: string, props: Gen2DeployProps = {}) => {
+  const noOutputTimeout = props?.timeoutMs ?? 10 * 60 * 1000;
+  const commandOptions = {
+    cwd,
+    stripColors: true,
+    noOutputTimeout,
+  };
+  // Run sandbox deployment
+  await
+    spawn(getNpxPath(), ['ampx', 'sandbox'], commandOptions)
+      .wait('Watching for file changes...')
+      .sendCtrlC()
+      .wait('Would you like to delete all the resources in your sandbox environment')
+      .sendNo()
+      .runAsync();
+}
+
+export const deleteSandbox = async (cwd: string) => {
+  const commandOptions = {
+    cwd,
+    stripColors: true,
+  };
+  await
+    spawn(getNpxPath(), ['ampx', 'sandbox', 'delete'], commandOptions)
+      .wait('Are you sure you want to delete all the resources in your sandbox environment')
+      .sendLine('Y')
+      .runAsync();
+}
