b4c4af5aeadab8f7794bc3c980e649a729103793
Dane Pilcher <dppilche@amazon.com>
2025-01-23 10:50:04 -0700

    fix: keep foreign keys on native platforms when they are part of the primary key (#887)

--- packages/appsync-modelgen-plugin/src/visitors/appsync-visitor.ts
+++ packages/appsync-modelgen-plugin/src/visitors/appsync-visitor.ts
@@ -1184,7 +1184,11 @@ export class AppSyncModelVisitor<
               connectionInfo.targetName !== 'id'
             ) {
               // Need to remove the field that is targetName
-              connectionInfo.targetNames.forEach(targetName => removeFieldFromModel(model, targetName));
+              const primaryKeyFieldNames = getModelPrimaryKeyComponentFields(model).map(field => field.name);
+              connectionInfo.targetNames
+                // Don't remove the field if it is part of the primary key field on the parent model
+                .filter(targetName => !primaryKeyFieldNames.includes(targetName))
+                .forEach(targetName => removeFieldFromModel(model, targetName));
             }
           });
         });
