9fabb5ad8c2546f9b385005a09924ac0c7989925
Bobby Yu <bobbyu@amazon.com>
2025-02-20 11:14:29 -0800

    not setup node version for _buildLinux and _buildWindows for now

--- shared-scripts.sh
+++ shared-scripts.sh
@@ -133,8 +133,6 @@ function _setShell {
 
 function _buildLinux {
   _setShell
-  echo "Setup Node Version $AMPLIFY_NODE_VERSION for Linux"
-  _setupNodeVersionLinux $AMPLIFY_NODE_VERSION
   echo "Linux Build"
   yarn run production-build
   storeCacheForLinuxBuildJob
@@ -142,8 +140,6 @@ function _buildLinux {
 
 function _buildWindows {
   echo "Linux Build"
-  echo "Setup Node Version $AMPLIFY_NODE_VERSION for Windows"
-  _setupNodeVersionWindows $AMPLIFY_NODE_VERSION
   yarn run production-build
   storeCacheForWindowsBuildJob
 }
