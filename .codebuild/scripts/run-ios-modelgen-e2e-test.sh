2d7bf66cd38dce1d685fabe039e8cbaf2a799d8e
Otavio Macedo <288203+otaviomacedo@users.noreply.github.com>
2026-03-04 09:54:59 +0000

    chore: increase iOS canary timeout
    This test trigger a GitHub workflow that is very reliable, and normally completes within the specified 25 min timeout. But we have seen cases recently where it took as long as 36 min to complete. Increasing the timeout to avoid unnecessary canary failures.

--- .codebuild/scripts/run-ios-modelgen-e2e-test.sh
+++ .codebuild/scripts/run-ios-modelgen-e2e-test.sh
@@ -52,7 +52,7 @@ main() {
     latest_run_id=$(get_latest_run_id)
     echo "Latest run ID: $latest_run_id"
     run_status=$(get_run_status "$latest_run_id")
-    timeout=$((SECONDS + 1500))  # 1500 seconds = 25 minutes
+    timeout=$((SECONDS + 2400))  # 2400 seconds = 40 minutes
 
     # Continuously check for status until completion
     while [[ "$run_status" != "completed"  && "$SECONDS" -lt "$timeout" ]]; do
